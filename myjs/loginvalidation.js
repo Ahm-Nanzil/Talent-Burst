8c794c82b439e7f45bf66c21a53519fc284e1c1f
ABU HASIB MUHAMMAD NANZIL <106665861+Ahm-Nanzil@users.noreply.github.com>
2023-10-20 01:30:25 +0600

    login, signup with js done

---
+++ myjs/loginvalidation.js
@@ -0,0 +1,39 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the form from submitting the traditional way
+
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var loginMessageElement = document.getElementById("loginMessage");
+
+    if (email.trim() === "" || password.trim() === "") {
+        loginMessageElement.textContent = "Please fill out all fields.";
+    } else {
+        loginMessageElement.textContent = ""; // Clear previous error message
+
+        // Prepare data to send to the server
+        var data = new FormData();
+        data.append("email", email);
+        data.append("password", password);
+
+        // Make an AJAX request to the login script
+        fetch("login.php", {
+            method: "POST",
+            body: data
+        })
+        .then(response => response.json()) // Assuming the PHP script returns JSON data
+        .then(data => {
+            // Handle the response from the server (data contains response from PHP script)
+            if (data.success) {
+                // Redirect to dashboard or perform other actions upon successful login
+                window.location.href = "index.html";
+            } else {
+                loginMessageElement.textContent = data.message; // Display login error message
+            }
+        })
+        .catch(error => {
+            // Handle errors that occurred during the fetch operation
+            loginMessageElement.textContent = "Error occurred during login.";
+            console.error(error);
+        });
+    }
+});
